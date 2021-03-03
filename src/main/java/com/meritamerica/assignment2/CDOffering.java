6d3d1552d5217d6d39da2211313838497f266d22
David Amateau <Damateeau@gmail.com>
2021-03-03 14:03:21 -0500

    Updating CDOffering.java

--- src/main/java/com/meritamerica/assignment2/CDOffering.java
+++ src/main/java/com/meritamerica/assignment2/CDOffering.java
@@ -1,17 +1,28 @@
 package com.meritamerica.assignment2;
 
 public class CDOffering {
+	/*
+	 * Instance Variables
+	 */
 	
+	private int term;
+	private double interestRate;
+	
+	
+	// This is the constructor for CDOffering
 	public CDOffering(int term, double interestRate) {
-		
+		this.term = term;
+		this.interestRate = interestRate;
 	}
 	
+	// This method will return the Term
 	public int getTerm() {
-		
+		return term;
 	}
 	
+	// This method will return the Interest Rate
 	double getInterestRate() {
-		
+		return interestRate;
 	}
 
 }
